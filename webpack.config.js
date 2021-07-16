23b4e5a9fc1b885f37d3073e01ace6a95544d024
gnwncpta <gnwncpta@gmail.com>
2021-07-16 22:27:26 +0700

    update code

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        aboutMe: './src/aboutMe.js'
+    },
     output: {
-        filename: 'index.bundled.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: '[name].bundled.js',
+        path: path.resolve(__dirname, 'public')
     },
     module: {
         rules: [
             { test: /\.css$/, use: ['style-loader', 'css-loader'] }
         ]
-    }
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            host: 3000,
+            server: { baseDir: ['public'] }
+        })
+    ]
 }
