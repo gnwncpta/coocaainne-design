2cec3e2cfca5b1fd67ae43e20bdb9b377c156a7d
gnwncpta <gnwncpta@gmail.com>
2021-07-17 03:13:10 +0700

    update code

--- src/artworks.js
+++ src/artworks.js
@@ -1,6 +1,12 @@
 import './artworks.css';
 
 const logo = document.querySelector('header img');
+const techStack = document.querySelector('footer .left a.tech');
+const designTools = document.querySelector('footer .left a.design');
+
+techStack.addEventListener('click', () => {
+    console.log('This user ask Tech Stack');
+})
 
 logo.addEventListener('click', () => {
     window.location.href = './index.html';
--- src/home.js
+++ src/home.js
@@ -1,6 +1,12 @@
 import './home.css';
 
 const logo = document.querySelector('header img');
+const hamburger = document.querySelector('header .hamburger');
+const menuOverlay = document.querySelector('header .hamburger .menu-overlay');
+
+hamburger.addEventListener('click', () => {
+    menuOverlay.classList.toggle('show');
+});
 
 logo.addEventListener('click', () => {
     window.location.href = 'index.html';
